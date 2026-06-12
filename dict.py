3090f55a5dd4903674baf0401e6ea4da033559e3
saieppalpelli <saieppalpelli@gmail.com>
2026-06-12 16:29:56 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,22 @@
+
+Raj={
+'name': 'Raj',
+'grades':'O',
+'attendance': '100'
+}
+print (Raj)
+Rohan={
+'name': 'Rohan',
+'grades': 'A',
+'attendance': '100'
+}
+Raj.update({'grades':'O'})
+print (Rohan)
+Viraj={
+'name': 'Viraj',
+'grades':'O',
+'attendance': '100'
+}
+print (Viraj)
+merge=(Raj, Rohan)
+print("Merged list:",merge)
